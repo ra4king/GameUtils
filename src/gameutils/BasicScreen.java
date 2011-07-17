8265187e37dfee71c6c7cd980fd4e3ed892fabdf
ra4king <devnull@localhost>
2011-07-17 02:21:41 -0400

    Removed Screen parameter in event methods in BasicScreen.

--- src/gameutils/BasicScreen.java
+++ src/gameutils/BasicScreen.java
@@ -9,7 +9,7 @@ import java.awt.event.MouseWheelEvent;
  * Takes care of most abstract methods.
  * @author Roi Atalla
  */
-public abstract class BasicScreen implements Screen, InputListener {
+public abstract class BasicScreen implements Screen {
 	private Game game;
 	
 	/**
@@ -17,7 +17,51 @@ public abstract class BasicScreen implements Screen, InputListener {
 	 */
 	public void init(Game game) {
 		this.game = game;
-		game.addInputListener(this, this);
+		game.addInputListener(this, new InputListener() {
+			public void keyPressed(KeyEvent key, Screen screen) {
+				BasicScreen.this.keyPressed(key);
+			}
+			
+			public void keyReleased(KeyEvent key, Screen screen) {
+				BasicScreen.this.keyReleased(key);
+			}
+			
+			public void keyTyped(KeyEvent key, Screen screen) {
+				BasicScreen.this.keyTyped(key);
+			}
+			
+			public void mouseEntered(MouseEvent me, Screen screen) {
+				BasicScreen.this.mouseEntered(me);
+			}
+			
+			public void mouseExited(MouseEvent me, Screen screen) {
+				BasicScreen.this.mouseExited(me);
+			}
+			
+			public void mousePressed(MouseEvent me, Screen screen) {
+				BasicScreen.this.mousePressed(me);
+			}
+			
+			public void mouseReleased(MouseEvent me, Screen screen) {
+				BasicScreen.this.mouseReleased(me);
+			}
+			
+			public void mouseClicked(MouseEvent me, Screen screen) {
+				BasicScreen.this.mouseClicked(me);
+			}
+			
+			public void mouseDragged(MouseEvent me, Screen screen) {
+				BasicScreen.this.mouseDragged(me);
+			}
+			
+			public void mouseMoved(MouseEvent me, Screen screen) {
+				BasicScreen.this.mouseMoved(me);
+			}
+			
+			public void mouseWheelMoved(MouseWheelEvent mwe, Screen screen) {
+				BasicScreen.this.mouseWheelMoved(mwe);
+			}
+		});
 	}
 	
 	public Game getGame() {
@@ -38,25 +82,25 @@ public abstract class BasicScreen implements Screen, InputListener {
 	
 	public abstract void draw(Graphics2D g);
 	
-	public void keyPressed(KeyEvent key, Screen screen) {}
+	public void keyPressed(KeyEvent key) {}
 	
-	public void keyReleased(KeyEvent key, Screen screen) {}
+	public void keyReleased(KeyEvent key) {}
 	
-	public void keyTyped(KeyEvent key, Screen screen) {}
+	public void keyTyped(KeyEvent key) {}
 	
-	public void mouseEntered(MouseEvent me, Screen screen) {}
+	public void mouseEntered(MouseEvent me) {}
 	
-	public void mouseExited(MouseEvent me, Screen screen) {}
+	public void mouseExited(MouseEvent me) {}
 	
-	public void mousePressed(MouseEvent me, Screen screen) {}
+	public void mousePressed(MouseEvent me) {}
 	
-	public void mouseReleased(MouseEvent me, Screen screen) {}
+	public void mouseReleased(MouseEvent me) {}
 	
-	public void mouseClicked(MouseEvent me, Screen screen) {}
+	public void mouseClicked(MouseEvent me) {}
 	
-	public void mouseDragged(MouseEvent me, Screen screen) {}
+	public void mouseDragged(MouseEvent me) {}
 	
-	public void mouseMoved(MouseEvent me, Screen screen) {}
+	public void mouseMoved(MouseEvent me) {}
 	
-	public void mouseWheelMoved(MouseWheelEvent mwe, Screen screen) {}
+	public void mouseWheelMoved(MouseWheelEvent mwe) {}
 }
