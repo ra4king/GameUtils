e5fb3add0fdea89064098bd71334d0ce54b0dec6
ra4king <devnull@localhost>
2011-08-24 23:56:59 -0400

    Made default init method with a Screen parameter final in GameComponent. Now there is an init method with a GameWorld parameter.

--- src/gameutils/gameworld/GameComponent.java
+++ src/gameutils/gameworld/GameComponent.java
@@ -1,6 +1,7 @@
 package gameutils.gameworld;
 
 import gameutils.Entity;
+import gameutils.Screen;
 
 public abstract class GameComponent extends Entity {
 	public GameComponent() {
@@ -10,6 +11,13 @@ public abstract class GameComponent extends Entity {
 	public GameComponent(double x, double y, double width, double height) {
 		super(x,y,width,height);
 	}
+	
+	public final void init(Screen screen) {
+		super.init(screen);
+		init((GameWorld)screen);
+	}
+	
+	public void init(GameWorld gameWorld) {}
 	
 	public GameWorld getParent() {
 		return (GameWorld)super.getParent();
