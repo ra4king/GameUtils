e75d74de156374111d9d46bebd9df55e22cd43da
ra4king <devnull@localhost>
2011-08-29 02:42:59 -0400

    Changed all methods in FastMath to double.

--- src/gameutils/util/FastMath.java
+++ src/gameutils/util/FastMath.java
@@ -1,116 +1,108 @@
 package gameutils.util;
 
 public class FastMath {
-	public static final float PI = (float)Math.PI;
-	public static final float E = (float)Math.E;
+	public static final double PI = Math.PI;
+	public static final double E = Math.E;
 	
-	private static final float RAD,DEG;
-	private static final float radToIndex, degToIndex;
+	private static final double RAD,DEG;
+	private static final double radToIndex, degToIndex;
 	private static final int SIN_MASK = ~(-1 << 12);
-	private static final float[] sin, cos;
+	private static final double[] sin, cos;
 	
 	private static final int ATAN2_COUNT = (~(-1 << (7 << 1))) + 1;
 	private static final int ATAN2_DIM = (int) Math.sqrt(ATAN2_COUNT);
-	private static final float[] atan2 = new float[ATAN2_COUNT];
+	private static final double[] atan2 = new double[ATAN2_COUNT];
 	
 	static {
-		RAD = (float)(Math.PI/180.0);
-		DEG = (float)(180.0/Math.PI);
+		RAD = (Math.PI/180.0);
+		DEG = (180.0/Math.PI);
 		
 		final int SIN_COUNT = SIN_MASK + 1;
 		
-		final float radFull    = PI * 2.0f;
+		final double radFull    = PI * 2.0;
 		radToIndex = SIN_COUNT/radFull;
-		degToIndex = SIN_COUNT/360.0f;
+		degToIndex = SIN_COUNT/360.0;
 		
-		sin = new float[SIN_COUNT];
-		cos = new float[SIN_COUNT];
+		sin = new double[SIN_COUNT];
+		cos = new double[SIN_COUNT];
 		
 		for(int i = 0; i < SIN_COUNT; i++) {
-			sin[i] = (float)Math.sin((i + 0.5)/SIN_COUNT * radFull);
-			cos[i] = (float)Math.cos((i + 0.5)/SIN_COUNT * radFull);
+			sin[i] = Math.sin((i + 0.5)/SIN_COUNT * radFull);
+			cos[i] = Math.cos((i + 0.5)/SIN_COUNT * radFull);
 		}
 		
 		for(int i = 0; i < 360; i += 90) {
-			sin[(int)(i * degToIndex) & SIN_MASK] = (float)Math.sin(i * Math.PI/180.0);
-			cos[(int)(i * degToIndex) & SIN_MASK] = (float)Math.cos(i * Math.PI/180.0);
+			sin[(int)(i * degToIndex) & SIN_MASK] = Math.sin(i * Math.PI/180.0);
+			cos[(int)(i * degToIndex) & SIN_MASK] = Math.cos(i * Math.PI/180.0);
 		}
 		
 		for(int i = 0; i < ATAN2_DIM; i++) {
 			for (int j = 0; j < ATAN2_DIM; j++) {
-				float x0 = (float)i/ATAN2_DIM;
-				float y0 = (float)j/ATAN2_DIM;
+				double x0 = i/ATAN2_DIM;
+				double y0 = j/ATAN2_DIM;
 				
-				atan2[j * ATAN2_DIM + i] = (float)Math.atan2(y0, x0);
+				atan2[j * ATAN2_DIM + i] = Math.atan2(y0, x0);
 			}
 		}
-	}
-	
-	public static final float toDegrees(float rad) {
-		return rad*DEG;
 	}
 	
 	public static final double toDegrees(double rad) {
 		return rad*DEG;
 	}
 	
-	public static final float toRadians(float deg) {
-		return deg*RAD;
-	}
-	
 	public static final double toRadians(double deg) {
 		return deg*RAD;
 	}
 	
-	public static final float sin(float rad) {
+	public static final double sin(double rad) {
 		return sin[(int)(rad * radToIndex) & SIN_MASK];
 	}
 	
-	public static final float cos(float rad) {
+	public static final double cos(double rad) {
 		return cos[(int)(rad * radToIndex) & SIN_MASK];
 	}
 	
-	public static final float sinDeg(float deg) {
+	public static final double sinDeg(double deg) {
 		return sin[(int)(deg * degToIndex) & SIN_MASK];
 	}
 	
-	public static final float cosDeg(float deg) {
+	public static final double cosDeg(double deg) {
 		return cos[(int) (deg * degToIndex) & SIN_MASK];
 	}
 	
-	public static final float atan2Deg(float y, float x) {
+	public static final double atan2Deg(double y, double x) {
 		return atan2(y, x) * DEG;
 	}
 	
-	public static final float atan2(float y, float x) {
-		float add, mul;
+	public static final double atan2(double y, double x) {
+		double add, mul;
 		
-		if (x < 0.0f) {
-			if (y < 0.0f) {
+		if (x < 0.0) {
+			if (y < 0.0) {
 				x = -x;
 				y = -y;
 				
-				mul = 1.0f;
+				mul = 1.0;
 			}
 			else {
 				x = -x;
-				mul = -1.0f;
+				mul = -1.0;
 			}
 			
 			add = -PI;
 		}
 		else {
-			if (y < 0.0f) {
+			if (y < 0.0) {
 				y = -y;
-				mul = -1.0f;
+				mul = -1.0;
 			}
 			else
-				mul = 1.0f;
+				mul = 1.0;
 			
-			add = 0.0f;
+			add = 0.0;
 		}
 		
-		float invDiv = (ATAN2_DIM-1) / ((x < y) ? y : x);
+		double invDiv = (ATAN2_DIM-1) / ((x < y) ? y : x);
 		
 		int xi = (int) (x * invDiv);
 		int yi = (int) (y * invDiv);
