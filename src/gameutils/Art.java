0f26cdfe21cbddbf6820903a169efab81a66801a
ra4king <devnull@localhost>
2011-11-05 16:03:13 -0400

    Created an Assets class that holds common functionality that Art and Sound share.

--- src/gameutils/Art.java
+++ src/gameutils/Art.java
@@ -5,9 +5,8 @@ import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.awt.Transparency;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -15,143 +14,16 @@ import javax.imageio.ImageIO;
  * A class that stores Images.
  * @author Roi Atalla
  */
-public class Art {
-	private Map<String,Image> images;
-	
-	Art() {
-		images = new HashMap<String,Image>();
+public class Art extends Assets<Image> {
+	public Image add(Image i, String name) {
+		return super.add(createCompatibleImage(i),name);
 	}
 	
-	/**
-	 * Returns the canonical name of this file path. It strips the path and the extension.
-	 * @param file The file to be canonicalized.
-	 * @return The canonical name of this file.
-	 */
-	public static String getFileName(String file) {
-		file = file.replace("\\","/");
-		return file.substring(file.lastIndexOf("/")+1,file.lastIndexOf("."));
+	public Image extract(URL url) throws IOException {
+		return ImageIO.read(url);
 	}
 	
-	/**
-	 * This method searches the root directory for the file path. Then it obtains the image by using <code>javax.imageio.ImageIO</code>.
-	 * Its associated name is its canonical name.
-	 * @param filename The file name of the image. Automatically creates a compatible image.
-	 * @return The Image itself.
-	 * @throws Exception
-	 */
-	public Image add(String file) {
-		return add(file,getFileName(file));
-	}
-	
-	/**
-	 * This method searches the root directory for the file path. Then it obtains the image by using <code>javax.imageio.ImageIO</code>.
-	 * @param filename The file name of the image. Automatically creates a compatible image.
-	 * @param name The name to associate with this image.
-	 * @return The Image imported.
-	 * @throws Exception
-	 */
-	public Image add(String file, String name) {
-		return add(getClass().getResource("/"+file),name);
-	}
-	
-	/**
-	 * Obtains the image using <code>javax.imageio.ImageIO</code> and adds it the map.
-	 * @param url The URL to the image. Automatically creates a compatible image.
-	 * @param name The name of the image.
-	 * @return The Image itself.
-	 * @throws Exception
-	 */
-	public Image add(URL url, String name) {
-		try{
-			return add(ImageIO.read(url),name);
-		}
-		catch(Exception exc) {
-			throw new IllegalArgumentException("Error loading image: " + url + " with name: " + name);
-		}
-	}
-	
-	/**
-	 * Adds the image to the map.
-	 * @param image The image to be added. Automatically creates a compatible version of it.
-	 * @param name The name of the image.
-	 * @return The Image itself.
-	 */
-	public synchronized Image add(Image image, String name) {
-		return images.put(name,createCompatibleImage(image));
-	}
-	
-	/**
-	 * Returns an image with the specified name.
-	 * @param name The name of the image.
-	 * @return The image associated with this name, if not found, returns null.
-	 */
-	public Image get(String name) {
-		return images.get(name);
-	}
-	
-	/**
-	 * Returns the name associated with this image.
-	 * @param image The image who's associated name is returned.
-	 * @return The name of the associated image. If it is not found, <code>null</code> is returned.
-	 */
-	public synchronized String getName(Image image) {
-		for(String s : images.keySet()) {
-			if(images.get(s) == image || images.get(s).equals(image))
-				return s;
-		}
-		
-		return null;
-	}
-	
-	/**
-	 * Renames the image with a new name.
-	 * @param oldName The image name to be renamed.
-	 * @param newName The new named to be used.
-	 */
-	public void rename(String oldName, String newName) {
-		add(remove(oldName),newName);
-	}
-	
-	/**
-	 * Does the same thing as add(Image,String) except it ensures that the image is replaced since add(Image,String) is unreliable due to String's internalization mechanism.
-	 * @param oldName The name of the previous image.
-	 * @param newImage The new image.
-	 * @return The old image.
-	 */
-	public Image replace(String oldName, Image newImage) {
-		if(get(oldName) == null)
-			throw new IllegalArgumentException("Invalid name");
-		
-		return add(newImage,oldName);
-	}
-	
-	/**
-	 * Swaps the images associated with both strings.
-	 * @param first The name of the first image.
-	 * @param second The name of the second image.
-	 */
-	public void swap(String first, String second) {
-		Image i = images.get(first);
-		Image i2 = images.get(second);
-		
-		if(i == null)
-			throw new IllegalArgumentException("First name is invalid.");
-		if(i2 == null)
-			throw new IllegalArgumentException("Second name is invalid");
-		
-		images.put(second,images.put(first,images.get(second)));
-	}
-	
-	/**
-	 * Removes the specified image name.
-	 * @param name The name of the image to be removed.
-	 * @return The image associated with this name, if not found, returns null.
-	 */
-	public synchronized Image remove(String name) {
-		Image i = images.get(name);
-		images.remove(name);
-		return i;
-	}
+	Art() {}
 	
 	/**
 	 * Splits the image with the specified width and height of each quadrant and adds the pieces to the map.
@@ -164,7 +36,7 @@ public class Art {
 		Image im[][] = split(image,width,height);
 		for(Image i[] : im)
 			for(Image i2 : i)
-				add(i2,"Image"+images.size());
+				add(i2,"Image"+size());
 		return im;
 	}
 	
@@ -257,95 +129,5 @@ public class Art {
 			g.drawImage(image,0,-image.getHeight(null),null);
 		g.dispose();
 		return newImage;
-	}
-	
-	/**
-	 * A convenience class that buffers images and loads them all at once.
-	 * @author Roi Atalla
-	 */
-	public class Loader implements Runnable {
-		private Map<String,String> files;
-		private int status;
-		
-		public Loader() {
-			files = new HashMap<String,String>();
-		}
-		
-		/**
-		 * Returns the total number of images in this buffer.
-		 * @return The total number of images in this buffer.
-		 */
-		public int getTotal() {
-			return files.size();
-		}
-		
-		/**
-		 * Returns the number of images added and loaded.
-		 * @return The number of images added and loaded.
-		 */
-		public int getStatus() {
-			return status;
-		}
-		
-		/**
-		 * Adds the file to the buffer.
-		 * @param file The file to be loaded. The associated name is its canonical name.
-		 */
-		public void addFile(String file) {
-			addFile(file,Art.getFileName(file));
-		}
-		
-		/**
-		 * Adds the file and sets the name associated with it.
-		 * @param file The file to be loaded.
-		 * @param name The name to be associated to this image.
-		 */
-		public synchronized void addFile(String file, String name) {
-			files.put(name,file);
-		}
-		
-		/**
-		 * A list of files to be added.
-		 * @param files The list of files to be added. Their associated names will be their canonical names.
-		 */
-		public void addFiles(String ... files) {
-			for(String s : files)
-				addFile(s);
-		}
-		
-		/**
-		 * A list of files to be added.
-		 * @param files The list of files to be added. Index 0 is the file path. Index 1 is the associated name.
-		 */
-		public void addFiles(String[] ... files) {
-			for(String[] s : files) {
-				addFile(s[0],s[1]);
-			}
-		}
-		
-		/**
-		 * Spawns a new thread and adds all images to the Art instance.
-		 */
-		public synchronized void start() {
-			new Thread(this).start();
-		}
-		
-		/**
-		 * Adds all images to the Art instance.
-		 */
-		public synchronized void run() {
-			for(String s : files.keySet()) {
-				try{
-					add(files.get(s),s);
-					status++;
-				}
-				catch(Exception exc) {
-					System.out.println(s);
-					exc.printStackTrace();
-					status = -1;
-					return;
-				}
-			}
-		}
 	}
 }
--- src/gameutils/Sound.java
+++ src/gameutils/Sound.java
@@ -1,8 +1,7 @@
 package gameutils;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
@@ -12,134 +11,18 @@ import javax.sound.sampled.FloatControl;
  * A class that stores sound clips.
  * @author Roi Atalla
  */
-public class Sound {
-	private Map<String,Clip> clips;
+public class Sound extends Assets<Clip> {
 	private volatile boolean on = true;
 	
-	Sound() {
-		clips = new HashMap<String,Clip>();
-	}
-	
-	/**
-	 * Returns the canonical name of this file path. It strips the path and the extension.
-	 * @param file The file to be canonicalized.
-	 * @return The canonical name of this file.
-	 */
-	public static String getFileName(String file) {
-		file = file.replace("\\","/");
-		return file.substring(file.lastIndexOf("/")+1,file.lastIndexOf("."));
-	}
-	
-	/**
-	 * This method searches the root directory for the file path. Then it opens a Clip on the specified file.
-	 * Its associated name is set as its canonical name.
-	 * @param file The file to import.
-	 * @return The Clip opened.
-	 */
-	public Clip add(String file) {
-		return add(file,getFileName(file));
-	}
-	
-	/**
-	 * This method searches the root directory for the file path. Then it opens a Clip on the specified file.
-	 * @param file The file to import.
-	 * @param name The name to associate with this Clip.
-	 * @return The Clip opened.
-	 */
-	public Clip add(String file, String name) {
-		return add(getClass().getResource("/"+file),name);
-	}
-	
-	/**
-	 * Opens a Clip on this URL.
-	 * @param url The URL of the sound file.
-	 * @param name The name to associate with this Clip.
-	 * @return The Clip opened.
-	 */
-	public Clip add(URL url, String name) {
+	public Clip extract(URL url) throws IOException {
 		try{
 			Clip clip = AudioSystem.getClip();
 			clip.open(AudioSystem.getAudioInputStream(url));
-			return add(clip,name);
+			return clip;
 		}
 		catch(Exception exc) {
-			throw new IllegalArgumentException("Error loading clip: " + url + " with name: " + name + ": " + exc);
+			throw new IOException("Error loading clip: " + url + ": " + exc);
 		}
-	}
-	
-	/**
-	 * Adds the Clip.
-	 * @param clip The Clip to add.
-	 * @param name The name to associate with this clip.
-	 * @return The clip that was added.
-	 */
-	public synchronized Clip add(Clip clip, String name) {
-		if(clip.isOpen())
-			clips.put(name,clip);
-		
-		return clip;
-	}
-	
-	/**
-	 * Returns the Clip with the specified name.
-	 * @param name The name of the Clip.
-	 * @return The Clip with the specified name.
-	 */
-	public Clip getClip(String name) {
-		return clips.get(name);
-	}
-	
-	/**
-	 * Renames the Clip with the specified name.
-	 * @param oldName The old name.
-	 * @param newName The new name.
-	 * @throws Exception
-	 */
-	public void rename(String oldName, String newName) {
-		add(remove(oldName),newName);
-	}
-	
-	/**
-	 * Replaces the Clip associated with the specified name with the specified Clip. Does the same thing as <code>add(Clip,String)</code>
-	 * @param oldName The name of the old Clip.
-	 * @param newClip The new Clip that will replace the old Clip.
-	 * @return The Clip that was replaced.
-	 */
-	public Clip replace(String oldName, Clip newClip) {
-		if(getClip(oldName) == null)
-			throw new IllegalArgumentException("Invalid name");
-		
-		Clip clip = getClip(oldName);
-		add(newClip,oldName);
-		return clip;
-	}
-	
-	/**
-	 * Swaps the Clips of both specified names.
-	 * @param first The first Clip.
-	 * @param second The second Clip.
-	 */
-	public synchronized void swap(String first, String second) {
-		Clip c = clips.get(first);
-		Clip c2 = clips.get(second);
-		
-		if(c == null)
-			throw new IllegalArgumentException("First name is invalid.");
-		if(c2 == null)
-			throw new IllegalArgumentException("Second name is invalid");
-		
-		clips.put(second,clips.put(first,clips.get(second)));
-	}
-	
-	/**
-	 * Removes the Clip.
-	 * @param name The name of the Clip.
-	 * @return The Clip that was removed.
-	 */
-	public synchronized Clip remove(String name) {
-		Clip c = getClip(name);
-		clips.remove(name);
-		return c;
 	}
 	
 	/**
@@ -147,15 +30,15 @@ public class Sound {
 	 * @param name The name of the Clip to play.
 	 */
 	public void play(String name) {
-		getClip(name).stop();
-		getClip(name).setMicrosecondPosition(0);
+		get(name).stop();
+		get(name).setMicrosecondPosition(0);
 		
 		if(!on) {
-			FloatControl volume = (FloatControl)getClip(name).getControl(FloatControl.Type.MASTER_GAIN);
+			FloatControl volume = (FloatControl)get(name).getControl(FloatControl.Type.MASTER_GAIN);
 			volume.setValue(volume.getMinimum());
 		}
 		
-		getClip(name).start();
+		get(name).start();
 	}
 	
 	/**
@@ -163,22 +46,22 @@ public class Sound {
 	 * @param name The name of the Clip to loop.
 	 */
 	public void loop(String name) {
-		getClip(name).stop();
-		getClip(name).setMicrosecondPosition(0);
+		get(name).stop();
+		get(name).setMicrosecondPosition(0);
 		
 		if(!on) {
-			FloatControl volume = (FloatControl)getClip(name).getControl(FloatControl.Type.MASTER_GAIN);
+			FloatControl volume = (FloatControl)get(name).getControl(FloatControl.Type.MASTER_GAIN);
 			volume.setValue(volume.getMinimum());
 		}
 		
-		getClip(name).loop(Clip.LOOP_CONTINUOUSLY);
+		get(name).loop(Clip.LOOP_CONTINUOUSLY);
 	}
 	
 	/**
 	 * Pauses all currently playing Clips.
 	 */
 	public synchronized void pause() {
-		for(Clip c : clips.values())
+		for(Clip c : assets.values())
 			c.stop();
 	}
 	
@@ -186,7 +69,7 @@ public class Sound {
 	 * Plays all currently paused Clips.
 	 */
 	public synchronized void resume() {
-		for(Clip c : clips.values()) {
+		for(Clip c : assets.values()) {
 			if(on && c.getMicrosecondPosition() != c.getMicrosecondLength() &&
 					c.getMicrosecondPosition() != 0) {
 				c.start();
@@ -199,7 +82,7 @@ public class Sound {
 	 * @param name The name of the Clip to pause.
 	 */
 	public void pause(String name) {
-		getClip(name).stop();
+		get(name).stop();
 	}
 	
 	/**
@@ -207,8 +90,8 @@ public class Sound {
 	 * @param name The name of the Clip to stop.
 	 */
 	public void stop(String name) {
-		getClip(name).stop();
-		getClip(name).setMicrosecondPosition(0);
+		get(name).stop();
+		get(name).setMicrosecondPosition(0);
 	}
 	
 	/**
@@ -218,7 +101,7 @@ public class Sound {
 	public synchronized void setOn(boolean isOn) {
 		on = isOn;
 		
-		for(Clip c : clips.values()) {
+		for(Clip c : assets.values()) {
 			FloatControl volume = (FloatControl)c.getControl(FloatControl.Type.MASTER_GAIN);
 			if(on)
 				volume.setValue(0);
@@ -233,89 +116,5 @@ public class Sound {
 	 */
 	public boolean isOn() {
 		return on;
-	}
-	
-	/**
-	 * A convenience class that buffers Clips and loads them all at once.
-	 * @author Roi Atalla
-	 */
-	public class Loader implements Runnable {
-		private Map<String,String> files = new HashMap<String,String>();
-		private int status;
-		
-		/**
-		 * Returns the total number of Clips in this buffer.
-		 * @return The total number of Clips in this buffer.
-		 */
-		public int getTotal() {
-			return files.size();
-		}
-		
-		/**
-		 * Returns the number of Clips added and loaded.
-		 * @return The number of Clips added and loaded.
-		 */
-		public int getStatus() {
-			return status;
-		}
-		
-		/**
-		 * Adds the file to the buffer.
-		 * @param file The file to be loaded. The associated name is its canonical name.
-		 */
-		public void addFile(String file) {
-			addFile(file,Sound.getFileName(file));
-		}
-
-		/**
-		 * Adds the file and sets the name associated with it.
-		 * @param file The file to be loaded.
-		 * @param name The name to be associated to this Clip.
-		 */
-		public synchronized void addFile(String file, String name) {
-			files.put(name,file);
-		}
-		
-		/**
-		 * A list of files to be added.
-		 * @param files The list of files to be added. Their associated names will be their canonical names.
-		 */
-		public void addFiles(String ... files) {
-			for(String s : files)
-				addFile(s);
-		}
-		
-		/**
-		 * A list of files to be added.
-		 * @param files The list of files to be added. Index 0 is the file path. Index 1 is the associated name.
-		 */
-		public void addFiles(String[] ... files) {
-			for(String[] s: files)
-				addFile(s[0],s[1]);
-		}
-		
-		/**
-		 * Spawns a new thread and adds all Clips to the Sound instance.
-		 */
-		public synchronized void start() {
-			new Thread(this).start();
-		}
-		
-		/**
-		 * Adds all Clips to the Sound instance.
-		 */
-		public synchronized void run() {
-			for(String s : files.keySet()) {
-				try{
-					add(files.get(s),s);
-					status++;
-				}
-				catch(Exception exc) {
-					exc.printStackTrace();
-					status = -1;
-					return;
-				}
-			}
-		}
 	}
 }
